d03916c78d8b56aa8dbd20f2353c41c960f06c8c
scrains <rainyscrains@gmail.com>
2016-04-03 21:59:09 -0700

    Added files via upload

---
+++ desktop/src/com/math/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.math.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.math.game.MathGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MathGame(), config);
+	}
+}
